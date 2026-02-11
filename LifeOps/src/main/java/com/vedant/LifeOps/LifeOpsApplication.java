89bbaea173734b8b81e516c7ae5498e806658836
Vedant Mule <muleyvedant4@gmail.com>
2026-02-11 21:51:25 +0530

    Initialized Spring Boot Project setup

---
+++ LifeOps/src/main/java/com/vedant/LifeOps/LifeOpsApplication.java
@@ -0,0 +1,13 @@
+package com.vedant.LifeOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeOpsApplication.class, args);
+	}
+
+}
---
+++ LifeOps/src/test/java/com/vedant/LifeOps/LifeOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedant.LifeOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
