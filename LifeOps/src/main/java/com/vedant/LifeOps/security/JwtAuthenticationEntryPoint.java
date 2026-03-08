f6c324f0e5df81dba8454a7fe8001f698c1a7275
Vedant Mule <muleyvedant4@gmail.com>
2026-03-08 13:57:48 +0530

    src/main/java/com/vedant/LifeOps/security/JwtAuthenticationEntryPoint.java feat(security): improve authentication entry point handling
    - Updated AuthEntryPointJwt for handling unauthorized access
    - Improved security error responses for invalid or missing JWT
    - Integrated better error messaging for protected endpoints# Please enter the commit message for your changes. Lines starting

--- LifeOps/src/main/java/com/vedant/LifeOps/security/JwtAuthenticationEntryPoint.java
+++ LifeOps/src/main/java/com/vedant/LifeOps/security/JwtAuthenticationEntryPoint.java
@@ -17,5 +17,8 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletRequest response, AuthenticationException authException)
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+    }
+
 }
