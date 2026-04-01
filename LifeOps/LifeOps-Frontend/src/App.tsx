27b7e622fb17585c9756dfe054ed418c7ff7d8cd
Vedant Mule <muleyvedant4@gmail.com>
2026-04-01 19:04:39 +0530

    - connected login page with AuthContext   - used loginUser funciton for API call   - added basic form handling for authentication   - ensured app is wrapped with AuthProvider

--- LifeOps/LifeOps-Frontend/src/App.tsx
+++ LifeOps/LifeOps-Frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import Login from './pages/Login';
+import Login from ".././src/pages/Login";
 import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+            <Dashboard/>
+            </ProtectedRoute>
+            }/>
 
         </Routes>
       </BrowserRouter>
--- LifeOps/LifeOps-Frontend/src/pages/Login.tsx
+++ LifeOps/LifeOps-Frontend/src/pages/Login.tsx
@@ -1,31 +1,87 @@
 import { useState } from "react";
-import { login } from "../services/authService";
 import { useAuth } from "../context/AuthContext";
 
-export default function Login() {
-  const [form, setForm] = useState({ username: "", password: "" });
-  const auth = useAuth();
-  if(!auth) { throw new Error("AuthContext not found. Wrap with AuthProvider.")}
-  const { loginUser } = auth;
+// export default function Login() {
+
+//   const { loginUser} = useAuth();
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+
+//   const [form, setForm] = useState({ username: "", password: "" });
+//   const auth = useAuth();
+//   if(!auth) { throw new Error("AuthContext not found. Wrap with AuthProvider.")}
 
 
 
-  const handleLogin = async () => {
-    const res = await login(form);
-    loginUser(res.accessToken);
-  };
 
-  return (  
-    <div>
-      <h2>Login</h2>
-      <input placeholder="Username"
-        onChange={(e) => setForm({ ...form, username: e.target.value })}
+//   const handleLogin = async (e:any) => {
+//     // e.preventDefault();
+//     // try{
+//     //     await loginUser(username, password);
+//     //     alert("Login successful!");
+
+
+//     // }catch(err){
+//     //   alert("Login failed!");
+//     // }
+//     const res = await login(form);
+//     loginUser(res.accessToken);
+//   };
+
+//   return (  
+//     <div>
+//       <h2>Login</h2>
+//       <input placeholder="Username"
+//         onChange={(e) => setForm({ ...form, username: e.target.value })}
+//       />
+//       <input type="password"
+//         placeholder="Password"
+//         onChange={(e) => setForm({ ...form, password: e.target.value })}
+//       />
+//       <button onClick={handleLogin}>Login</button>
+//     </div>
+//   );
+// }
+
+
+const Login = () => {
+  const {loginUser} = useAuth();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e:any) => {
+
+      e.preventDefault();
+      try{
+        await loginUser(username, password);
+        alert("Login successful!");
+
+      }catch(err){
+      alert("Login failed");
+    }
+      
+    }
+    
+    
+return (
+    <form onSubmit={handleLogin}>
+      <input
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <input type="password"
-        placeholder="Password"
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      <input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+      <button type="submit">Login</button>
+    </form>
   );
-}
+
+  }
+
+  export default Login;
